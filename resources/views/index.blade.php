e5baccfb3e2ff707c5c17beb5a69237db1bf8a38
DekkoBraze <greende556@gmail.com>
2023-10-20 18:44:18 +0300

    first commit

---
+++ resources/views/index.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>MyUniverse</title>
+</head>
+<body>
+<h1>Добро пожаловать на MyUniverse!</h1>
+<h2>Здесь вы сможете оценивать все, что угодно, и делиться своими достижениями с друзьями!</h2>
+</body>
+</html>
